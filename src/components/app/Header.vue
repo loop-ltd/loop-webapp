ba4e854e7b76bd5692b8d2e74da9c80e99d22dcb
Dharmatologist <44667896+Dharmatologist@users.noreply.github.com>
2023-09-14 14:32:00 +0100

    Fake nav

--- src/components/app/Header.vue
+++ src/components/app/Header.vue
@@ -3,10 +3,10 @@
 </script>
 <template>
   <div class="bg-white h-[40px] flex items-center justify-between px-2 py-4">
-    <div class="flex items-align space-x-1">
+    <a href="/#" class="flex items-align space-x-1">
       <ChevronLeftIcon class="text-loop-600 w-6 h-6"></ChevronLeftIcon>
       <span class="font-bold text-loop-600">Back</span>
-    </div>
+    </a>
     <div class="flex items-center space-x-2">
       <img src="/logo.png" class="w-6 h-6" />
       <span class="text-uigray-900 font-bold">Loop</span>
--- src/components/app/HeaderDark.vue
+++ src/components/app/HeaderDark.vue
@@ -12,12 +12,16 @@
     class="shrink-0 bg-gradient-to-br from-loopgray-500 to-loopgray-100 h-[80px]"
     :style="{clipPath: clip}">
     <div class="h-[40px] flex items-center justify-between px-4 mt-[10px]">
-      <Avatar name="Edwin Groenendaal" size="sm"></Avatar>
+      <a href="/#/cards">
+        <Avatar name="Edwin Groenendaal" size="sm"></Avatar>
+      </a>
       <div class="flex items-center space-x-2">
         <img src="/logo.png" class="w-6 h-6" />
         <span class="text-loop-600 font-bold">Loop</span>
       </div>
-      <Bars3Icon class="h-6 w-6 text-white" />
+      <a href="/#/onboarding">
+        <Bars3Icon class="h-6 w-6 text-white" />
+      </a>
     </div>
   </div>
 </template>
